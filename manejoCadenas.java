43705970355c10231c3f39b6fe706ae4607495b6
florestlv1999 <49159786+florestlv1999@users.noreply.github.com>
2019-04-01 17:07:49 +0200

    Add files via upload

---
+++ manejoCadenas.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC12-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       String nombre="fabrcio"; 
+       String apellido="cahuana";
+       System.out.println("concatenacion:"+nombre+apellido);
+        System.out.println("nueva linea:\n"+nombre+" "+apellido);
+        System.out.println("tabulador:\t"+nombre+" "+apellido);
+        System.out.println("retorceso:\b"+nombre+" "+apellido);
+        System.out.println("retorno de carro:\r"+nombre+" "+apellido);
+        System.out.println("comilla simple:\'"+nombre+" "+apellido);
+        System.out.println("comilla adoble:\""+nombre+" "+apellido);
+        System.out.println("retroceso:\b\b"+nombre+" "+apellido);
+        
+      
+      
+       
+       {
+       
+    }
+    
+
