70b879bfe1bd27ebf8c7a0f9f76699cf0f4b8961
florestlv1999 <49159786+florestlv1999@users.noreply.github.com>
2019-05-14 19:22:03 +0200

    Add files via upload

---
+++ david cahuana/src/david/cahuana/DavidCahuana.java
@@ -0,0 +1,16 @@
+package personas;
+public class Persona {
+//Atributos de una clase
+//No es necesario asignar valores
+String nombre;
+String apellidoPaterno;
+String apellidoMaterno;
+string sexo; 
+//Metodos de la clase
+//Los usaran los objetos de esta clase
+public void desplegarNombre() {
+System.out.println("Nombre : " + nombre);
+System.out.println("Apellido Paterno : " + apellidoPaterno);
+System.out.println("Apellido Materno : " + apellidoMaterno);
+}
+}
